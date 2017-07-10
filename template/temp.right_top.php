b46ebe4348b84d8fbb398760af3bad1ddf42b3f6
kacevnik <kacevnik@yandex.ru>
2017-07-10 18:18:02 +0300

    Добавил прокод в хедере

--- template/temp.right_top.php
+++ template/temp.right_top.php
@@ -6,6 +6,7 @@
 						<a class="navbar-minimalize minimalize btn btn-primary" href="#"><i class="fa fa-bars"></i></a>
 					</div>
 					<ul class="nav navbar-top-links navbar-right">
+						<li class="right_top_promo_kod">Промо код на скидку 10%: <b>B4573CB5</b></li>
 						<li class="right_top_cel">Цель: 100$/день</li>
 					</ul>
 				</nav>
